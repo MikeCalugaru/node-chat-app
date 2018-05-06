0f15111e49ebaa31837f1007df40323809177728
Mircea Calugaru <mircea.calugaru@gmail.com>
2018-05-06 19:41:17 +0100

    Add new message and create new message events

--- server/server.js
+++ server/server.js
@@ -11,12 +11,33 @@ var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-io.on('connection', (socket) => {
+io.on('connection', function(socket) {
   console.log('New user connected');
   
-  socket.on('disconnect', (socket) => {
+//  socket.emit('newEmail', {
+//    from: 'mike@example.com',
+//    text: 'Hey. What is going on',
+//    createdAt: 123
+//  });
+  
+  socket.on('disconnect', function(socket) {
     console.log('Client disconnected');
   });
+  
+//  socket.on('createEmail', (newEmail) => {
+//    console.log('createEmail', newEmail);
+//  });
+  
+  socket.emit('newMessage', {
+    from: 'userAndrew',
+    text: 'Hello user, let\' meet at 6',
+    createdAt: 123
+  });
+  
+  socket.on('createNewMessage', function(newMessage) {
+    console.log(newMessage);
+  });
+  
 });
 
 
