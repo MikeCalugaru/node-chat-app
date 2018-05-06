d94eb756b2c3bbadd34303b26c500802b9ed3983
Mircea Calugaru <mircea.calugaru@gmail.com>
2018-05-06 22:58:29 +0100

    Great new user, and alert others

--- server/server.js
+++ server/server.js
@@ -14,6 +14,15 @@ app.use(express.static(publicPath));
 io.on('connection', function(socket) {
   console.log('New user connected');
   
+  socket.emit('newMessage', {
+    from: 'admin',
+    text: 'Welcome to the chat app'
+  });
+  socket.broadcast.emit('newMessage', {
+    from: 'admin',
+    text: 'New user joined'
+  });
+  
   socket.on('createMessage', function(message) {
     console.log('Create message', message);
     io.emit('newMessage', {
@@ -21,6 +30,11 @@ io.on('connection', function(socket) {
       text: message.text,
       createdAt: new Date().getTime()
     });
+//    socket.broadcast.emit('newMessage', {
+//      from: message.from,
+//      text: message.text,
+//      createdAt: new Date().getTime()
+//    });
   });
   
    socket.on('disconnect', function(socket) {
